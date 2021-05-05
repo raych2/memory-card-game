b5d2a4c13fff423b58474003b991e916498a202d
raych2 <rtongco@gmail.com>
2021-05-04 23:13:11 -0700

    Add imageData.js to organize image data for cards

---
+++ src/imageData.js
@@ -0,0 +1,45 @@
+import mischievous from "./images/png/001-unicorn.png";
+import sleeping from "./images/png/002-unicorn-1.png";
+import winking from "./images/png/003-unicorn-2.png";
+import crying from "./images/png/004-unicorn-3.png";
+import mermaid from "./images/png/005-unicorn-4.png";
+import dancing from "./images/png/006-unicorn-5.png";
+import rich from "./images/png/007-unicorn-6.png";
+import sweaty from "./images/png/008-unicorn-7.png";
+import shocked from "./images/png/009-unicorn-8.png";
+import wine from "./images/png/010-unicorn-9.png";
+import question from "./images/png/011-unicorn-11.png";
+import joy from "./images/png/012-unicorn-12.png";
+import run from "./images/png/013-unicorn-13.png";
+import vomit from "./images/png/014-unicorn-14.png";
+import cool from "./images/png/015-unicorn-15.png";
+import kiss from "./images/png/016-unicorn-16.png";
+import angry from "./images/png/017-unicorn-17.png";
+import haunted from "./images/png/018-unicorn-10.png";
+import annoyed from "./images/png/019-unicorn-18.png";
+import pizza from "./images/png/020-unicorn-19.png";
+
+const unicorns = [
+  { name: mischievous, id: 0 },
+  { name: sleeping, id: 1 },
+  { name: winking, id: 2 },
+  { name: crying, id: 3 },
+  { name: mermaid, id: 4 },
+  { name: dancing, id: 5 },
+  { name: rich, id: 6 },
+  { name: sweaty, id: 7 },
+  { name: shocked, id: 8 },
+  { name: wine, id: 9 },
+  { name: question, id: 10 },
+  { name: joy, id: 11 },
+  { name: run, id: 12 },
+  { name: vomit, id: 13 },
+  { name: cool, id: 14 },
+  { name: kiss, id: 15 },
+  { name: angry, id: 16 },
+  { name: haunted, id: 17 },
+  { name: annoyed, id: 18 },
+  { name: pizza, id: 19 },
+];
+
+export default unicorns;
