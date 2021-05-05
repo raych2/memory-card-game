a2303070671955f8531fd555a5cc6f03289f656e
raych2 <rtongco@gmail.com>
2021-05-05 14:54:52 -0700

    Move imageData into images folder

---
+++ src/images/imageData.js
@@ -0,0 +1,45 @@
+import mischievous from "./png/001-unicorn.png";
+import sleeping from "./png/002-unicorn-1.png";
+import winking from "./png/003-unicorn-2.png";
+import crying from "./png/004-unicorn-3.png";
+import mermaid from "./png/005-unicorn-4.png";
+import dancing from "./png/006-unicorn-5.png";
+import rich from "./png/007-unicorn-6.png";
+import sweaty from "./png/008-unicorn-7.png";
+import shocked from "./png/009-unicorn-8.png";
+import wine from "./png/010-unicorn-9.png";
+import question from "./png/011-unicorn-11.png";
+import joy from "./png/012-unicorn-12.png";
+import run from "./png/013-unicorn-13.png";
+import vomit from "./png/014-unicorn-14.png";
+import cool from "./png/015-unicorn-15.png";
+import kiss from "./png/016-unicorn-16.png";
+import angry from "./png/017-unicorn-17.png";
+import haunted from "./png/018-unicorn-10.png";
+import annoyed from "./png/019-unicorn-18.png";
+import pizza from "./png/020-unicorn-19.png";
+
+const unicorns = [
+  { name: mischievous, id: 0 },
+  { name: sleeping, id: 1 },
+  { name: winking, id: 2 },
+  { name: crying, id: 3 },
+  { name: mermaid, id: 4 },
+  { name: dancing, id: 5 },
+  { name: rich, id: 6 },
+  { name: sweaty, id: 7 },
+  { name: shocked, id: 8 },
+  { name: wine, id: 9 },
+  { name: question, id: 10 },
+  { name: joy, id: 11 },
+  { name: run, id: 12 },
+  { name: vomit, id: 13 },
+  { name: cool, id: 14 },
+  { name: kiss, id: 15 },
+  { name: angry, id: 16 },
+  { name: haunted, id: 17 },
+  { name: annoyed, id: 18 },
+  { name: pizza, id: 19 },
+];
+
+export default unicorns;
