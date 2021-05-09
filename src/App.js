f7cafa364440ee990f6ed78bd6e25bd338c2fdd4
raych2 <rtongco@gmail.com>
2021-05-08 19:49:16 -0700

    Add header and footer to App

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import Game from "./components/Game";
 import "./App.css";
 
 function App() {
-  return <div className="App">
-    <Game />
-  </div>;
+  return (
+    <div className="App">
+      <h1>Memory Card Game</h1>
+      <h3>
+        Can you choose all 20 unicorns without choosing the same unicorn twice?
+      </h3>
+      <Game />
+      <footer>
+        <div id="icon">
+          Icons made by{" "}
+          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+            Freepik
+          </a>{" "}
+          from{" "}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
